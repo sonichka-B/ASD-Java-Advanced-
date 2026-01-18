707555308c193e5b66198917a35291e46b50bf54
Bordachenko Sofiia <s.bordachenko@gmail.com>
2026-01-18 21:25:20 +0200

    Hw1:add array and downcast in Main(task 3)
    Signed-off-by: Bordachenko Sofiia <s.bordachenko@gmail.com>

--- src/main/java/Homework1/Main.java
+++ src/main/java/Homework1/Main.java
@@ -1,14 +1,35 @@
 package Homework1;
 
 public class Main {
-   public static void main (String[] args){
-       Dog d1 = new Dog("Archi", 23, true,"wolf");
-       Dog d2 = new Dog("Siso", 13, false,"wolf");
+   public static void main (String[] args) {
+       Dog d1 = new Dog("Archi", 23, true, "wolf");
+       Dog d2 = new Dog("Siso", 13, false, "wolf");
 //       Dog d3 = new Dog("Siso", 13, false,"wolf");
 
-       System.out.println(d1==d2);
+       System.out.println(d1 == d2);
        System.out.println(d1.equals(d2));
+
+       Cat c1 = new Cat("Koko", 2, true, "ginger");
+       Cat c2 = new Cat("Gojo", 8, true, "black");
+
+       Animal[] animals = {d1, d2, c1, c2};
+       for (Animal animal : animals) System.out.println(animal.toString());
+
+        feed(animals[2]);
+        feed(animals[1]);
    }
+       public static void feed(Animal animal){
+           animal.feed();
+        if(animal instanceof Dog){
+            Dog dog = (Dog) animal;
+            dog.makeSound();
+        }
+           else if(animal instanceof Cat){
+               Cat cat = (Cat) animal;
+               cat.makeSound();
+           }
+
+       }
 
 
 
